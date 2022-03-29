10cc0225d59757152a23cf1ab7cd73e31a48cf66
k3kit <hadouken7809@gmail.com>
2022-03-29 14:19:17 +0300

    fix: fix test

--- module-1/src/components/card/Card-list.test.tsx
+++ module-1/src/components/card/Card-list.test.tsx
@@ -9,12 +9,26 @@ describe('test ', () => {
     const cards = screen.getAllByTestId('card');
     expect(cards).toHaveLength(data.length);
   });
+  test('list snapshot', () => {
+    const list = render(<CardList />);
+    expect(list).toMatchSnapshot();
+  });
 });
 
 describe('test ', () => {
   test(' one card test', () => {
-    render(<CardList />);
-    expect(screen.getByText(/Ignaz/)).toBeInTheDocument();
-    expect(screen.getByText(/ibrasted0@arizona.edu/)).toBeInTheDocument();
+    render(
+      <Card
+        avatar={'https://robohash.org/culpamagnameius.png?size=50x50&set=set1'}
+        first_name={'abc'}
+        email={'mail'}
+        gender={'gen'}
+      />
+    );
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByText(/abc/)).toBeInTheDocument();
+    expect(screen.getByText(/mail/)).toBeInTheDocument();
+    expect(screen.getByText(/gen/)).toBeInTheDocument();
+    screen.debug();
   });
 });
